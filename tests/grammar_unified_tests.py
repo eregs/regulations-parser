3a57e79374604bbe81171d03ef0a6cb207d3abc4
Will Barton <william.barton@cfpb.gov>
2015-06-29 09:43:07 -0400

    Add grammar to read specific parts from multi-part notices
    This is to ensure that a multi-date notice that we split up on date
    actually matches the CFR part we want.
    
    Conflicts:
    	tests/grammar_unified_tests.py

--- tests/grammar_unified_tests.py
+++ tests/grammar_unified_tests.py
@@ -14,7 +14,6 @@ class GrammarCommonTests(TestCase):
         self.assertEqual('A', result.p4)
         self.assertEqual('2', result.p5)
 
-
     def test_marker_subpart_title(self):
         # Typical case:
         text = u'Subpart K\u2014Exportation'
@@ -28,3 +27,13 @@ class GrammarCommonTests(TestCase):
         self.assertEqual(u'[Reserved]', result.subpart_title)
         self.assertEqual(u'J', result.subpart)
 
+    def test_notice_cfr_p(self):
+        text = '12 CFR Parts 1002, 1024, and 1026'
+        result = notice_cfr_p.parseString(text)
+        self.assertEqual(['1002', '1024', '1026'], list(result))
+        text = '12 CFR Parts 1024, and 1026'
+        result = notice_cfr_p.parseString(text)
+        self.assertEqual(['1024', '1026'], list(result))
+        text = '12 CFR Parts 1024'
+        result = notice_cfr_p.parseString(text)
+        self.assertEqual(['1024'], list(result))
