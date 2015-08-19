1bff597a4059f33d75cdb285d221cebf7e86bbe2
Tadhg O'Higgins <tadhg.ohiggins@gsa.gov>
2015-08-19 10:16:40 -0700

    Strip unsupported regex class from parsing of section numbers.

--- regparser/tree/xml_parser/reg_text.py
+++ regparser/tree/xml_parser/reg_text.py
@@ -224,7 +224,7 @@ def build_from_section(reg_part, section_xml):
 
     nodes = []
     section_nums = []
-    for match in re.finditer(r'%s\.(\d+[:alpha:]*)' % reg_part, section_no):
+    for match in re.finditer(r'%s\.(\d+[a-z]*)' % reg_part, section_no):
         secnum_candidate = match.group(1)
         if secnum_candidate.isdigit():
             secnum_candidate = int(secnum_candidate)
