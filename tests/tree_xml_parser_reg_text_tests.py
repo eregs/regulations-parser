e6fbdb3dbbe90a5f24662f2e61539d096c558d84
CM Lubinski <christopher.lubinski@gsa.gov>
2015-08-19 00:33:18 +0000

    Allow nodes without a marker to have the MARKERLESS paragraph type

--- tests/tree_xml_parser_reg_text_tests.py
+++ tests/tree_xml_parser_reg_text_tests.py
@@ -4,6 +4,7 @@ from unittest import TestCase
 from lxml import etree
 from mock import patch
 
+from regparser.tree.depth import markers as mtypes
 from regparser.tree.xml_parser import reg_text
 
 
@@ -335,6 +336,19 @@ class RegTextTest(TestCase):
         self.assertEqual(('A', ('(A) aaaa. ', '(A) aaaa. ')), a)
         self.assertEqual(('<E T="03">1</E>', ('(1) 1111',
                                               '(<E T="03">1</E>) 1111')), a1)
+
+    def test_get_markers_and_text_deceptive_single(self):
+        """Don't treat a single marker differently than multiple, there might
+        be prefix text"""
+        node = etree.fromstring('<P>Some words then (a) a subparagraph</P>')
+        results = reg_text.get_markers_and_text(node, ['a'])
+        self.assertEqual(len(results), 2)
+        prefix, subpar = results
+
+        self.assertEqual(prefix[0], mtypes.MARKERLESS)
+        self.assertEqual(prefix[1][0], 'Some words then ')
+        self.assertEqual(subpar[0], 'a')
+        self.assertEqual(subpar[1][0], '(a) a subparagraph')
 
     def test_get_markers_bad_citation(self):
         text = '(vi)<E T="03">Keyterm.</E>The information required by '
