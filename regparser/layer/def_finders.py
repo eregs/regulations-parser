f9b8a622d439c9ba198256e534b16d3da8ba6e4c
CM Lubinski <christopher.lubinski@gsa.gov>
2015-09-09 20:12:14 +0000

    Use an abstract base class for definition finders

--- regparser/layer/def_finders.py
+++ regparser/layer/def_finders.py
@@ -1,5 +1,6 @@
 # vim: set fileencoding=utf-8
 """Parsers for finding a term that's being defined within a node"""
+import abc
 from itertools import chain
 import re
 
@@ -27,7 +28,19 @@ class Ref(object):
             repr(self.term), repr(self.label), repr(self.start))
 
 
-class ExplicitIncludes(object):
+class FinderBase(object):
+    """Base class for all of the definition finder classes. Defines the
+    interface they must implement"""
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def find(self, node):
+        """Given a Node, pull out any definitions it may contain as a list of
+        Refs"""
+        raise NotImplementedError()
+
+
+class ExplicitIncludes(FinderBase):
     """Definitions can be explicitly included in the settings. For example,
     say that a paragraph doesn't indicate that a certain phrase is a
     definition; we can define INCLUDE_DEFINITIONS_IN in our settings file,
@@ -43,7 +56,7 @@ class ExplicitIncludes(object):
         return refs
 
 
-class SmartQuotes(object):
+class SmartQuotes(FinderBase):
     """Definitions indicated via smart quotes"""
     def __init__(self, stack):
         """Stack (which references ancestors of a node) is used to determine
@@ -73,7 +86,7 @@ class SmartQuotes(object):
         return False
 
 
-class ScopeMatch(object):
+class ScopeMatch(FinderBase):
     """We know these will be definitions because the scope of the definition
     is spelled out. E.g. 'for the purposes of XXX, the term YYY means'"""
     def __init__(self, finder):
@@ -94,7 +107,7 @@ class ScopeMatch(object):
         return refs
 
 
-class XMLTermMeans(object):
+class XMLTermMeans(FinderBase):
     """Namespace for a matcher for e.g. '<E>XXX</E> means YYY'"""
     def __init__(self, existing_refs=None):
         """Existing refs will be used to exclude certain matches"""
