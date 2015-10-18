464fa182525c7ac5e4dea0ee04944f3e0301a934
CM Lubinski <christopher.lubinski@gsa.gov>
2015-10-18 03:39:42 +0000

    Fix failing test after Path-to-Entry conversion

--- tests/eregs_index_tests.py
+++ tests/eregs_index_tests.py
@@ -1,94 +1,90 @@
+from contextlib import contextmanager
 from datetime import date
 import os
-import shutil
-import tempfile
 from time import time
 from unittest import TestCase
+
+from click.testing import CliRunner
 
 from regparser import eregs_index
 from regparser.history.versions import Version
 
 
-class SetupMixin(object):
-    """Change the eregs_index.ROOT to a tempdir"""
-    def setUp(self):
-        self._original_root = eregs_index.ROOT
-        eregs_index.ROOT = tempfile.mkdtemp()
-
-    def tearDown(self):
-        shutil.rmtree(eregs_index.ROOT)
-        eregs_index.ROOT = self._original_root
-
-
-class VersionEntryTests(SetupMixin, TestCase):
+class VersionEntryTests(TestCase):
     def test_iterator(self):
         """Versions should be correctly linearized"""
-        path = eregs_index.VersionEntry("12", "1000")
-        v1 = Version('1111', effective=date(2004, 4, 4),
-                     published=date(2004, 4, 4))
-        v2 = Version('2222', effective=date(2002, 2, 2),
-                     published=date(2004, 4, 4))
-        v3 = Version('3333', effective=date(2004, 4, 4),
-                     published=date(2003, 3, 3))
-        (path / '1111').write(v1)
-        (path / '2222').write(v2)
-        (path / '3333').write(v3)
+        with CliRunner().isolated_filesystem():
+            path = eregs_index.VersionEntry("12", "1000")
+            v1 = Version('1111', effective=date(2004, 4, 4),
+                         published=date(2004, 4, 4))
+            v2 = Version('2222', effective=date(2002, 2, 2),
+                         published=date(2004, 4, 4))
+            v3 = Version('3333', effective=date(2004, 4, 4),
+                         published=date(2003, 3, 3))
+            (path / '1111').write(v1)
+            (path / '2222').write(v2)
+            (path / '3333').write(v3)
 
-        self.assertEqual(['2222', '3333', '1111'], list(path))
+            self.assertEqual(['2222', '3333', '1111'], list(path))
 
 
-class DependencyGraphTests(SetupMixin, TestCase):
-    def setUp(self):
-        super(DependencyGraphTests, self).setUp()
-        self.dgraph = eregs_index.DependencyGraph()
-        path = eregs_index.Entry('path')
-        self.depender = path / 'depender'
-        self.dependency = path / 'dependency'
+class DependencyGraphTests(TestCase):
+    @contextmanager
+    def dependency_graph(self):
+        with CliRunner().isolated_filesystem():
+            path = eregs_index.Entry('path')
+            self.depender = path / 'depender'
+            self.dependency = path / 'dependency'
+            yield eregs_index.DependencyGraph()
 
     def test_nonexistent_files_are_stale(self):
         """By definition, if a file is not present, it needs to be rebuilt"""
-        self.dependency.write('value')
-        self.dgraph.add(self.depender, self.dependency)
-        self.assertFalse(self.dgraph.is_stale(self.dependency))
-        self.assertTrue(self.dgraph.is_stale(self.depender))
-        # shouldn't raise an exception; all dependencies are up to date
-        self.dgraph.validate_for(self.depender)
+        with self.dependency_graph() as dgraph:
+            self.dependency.write('value')
+            dgraph.add(self.depender, self.dependency)
+            self.assertFalse(dgraph.is_stale(self.dependency))
+            self.assertTrue(dgraph.is_stale(self.depender))
+            # shouldn't raise an exception; all dependencies are up to date
+            dgraph.validate_for(self.depender)
 
     def test_nonexistant_deps_are_stale(self):
         """If a dependency is not present, we're stale"""
-        self.depender.write('value')
-        self.dgraph.add(self.depender, self.dependency)
-        self.assertTrue(self.dgraph.is_stale(self.dependency))
-        self.assertTrue(self.dgraph.is_stale(self.depender))
-        with self.assertRaises(eregs_index.DependencyException):
-            self.dgraph.validate_for(self.depender)
+        with self.dependency_graph() as dgraph:
+            self.depender.write('value')
+            dgraph.add(self.depender, self.dependency)
+            self.assertTrue(dgraph.is_stale(self.dependency))
+            self.assertTrue(dgraph.is_stale(self.depender))
+            with self.assertRaises(eregs_index.DependencyException):
+                dgraph.validate_for(self.depender)
 
     def test_updates_to_dependencies_flow(self):
         """If a dependency is updated, the graph should be recalculated"""
-        self.dependency.write('value')
-        self.depender.write('value2')
-        self.dgraph.add(self.depender, self.dependency)
-        self.assertFalse(self.dgraph.is_stale(self.dependency))
-        self.assertFalse(self.dgraph.is_stale(self.depender))
+        with self.dependency_graph() as dgraph:
+            self.dependency.write('value')
+            self.depender.write('value2')
+            dgraph.add(self.depender, self.dependency)
+            self.assertFalse(dgraph.is_stale(self.dependency))
+            self.assertFalse(dgraph.is_stale(self.depender))
 
-        # Set the update time of the dependency to the future
-        os.utime(str(self.dependency),
-                 (time()*1000 + 1000, time()*1000 + 1000))
-        self.dgraph.dag.run()
-        self.assertFalse(self.dgraph.is_stale(self.dependency))
-        self.assertTrue(self.dgraph.is_stale(self.depender))
+            # Set the update time of the dependency to the future
+            os.utime(str(self.dependency),
+                     (time()*1000 + 1000, time()*1000 + 1000))
+            dgraph.dag.run()
+            self.assertFalse(dgraph.is_stale(self.dependency))
+            self.assertTrue(dgraph.is_stale(self.depender))
 
     def test_dependencies_serialized(self):
         """Every instance of DependencyGraph shares a serialized copy of the
         dependencies"""
-        self.dgraph.add(self.depender, self.dependency / '1')
-        self.dgraph.add(self.depender, self.dependency / '2')
-        self.assertEqual(
-            self.dgraph.graph[str(self.depender)],
-            set([str(self.dependency / 1), str(self.dependency / 2)]))
+        with self.dependency_graph() as dgraph:
+            dgraph.add(self.depender, self.dependency / '1')
+            dgraph.add(self.depender, self.dependency / '2')
+            self.assertEqual(
+                dgraph.graph[str(self.depender)],
+                set([str(self.dependency / 1), str(self.dependency / 2)]))
 
-        del self.dgraph     # implicitly closing the database
-        self.dgraph = eregs_index.DependencyGraph()
-        self.assertEqual(
-            self.dgraph.graph[str(self.depender)],
-            set([str(self.dependency / 1), str(self.dependency / 2)]))
+            del dgraph     # implicitly closing the database
+            dgraph = eregs_index.DependencyGraph()
+            self.assertEqual(
+                dgraph.graph[str(self.depender)],
+                set([str(self.dependency / 1), str(self.dependency / 2)]))
