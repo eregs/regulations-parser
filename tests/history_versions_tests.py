a8054b5db667dc4764f5aa73404dd0a043530dc8
CM Lubinski <christopher.lubinski@gsa.gov>
2015-11-25 01:12:04 +0000

    More useful test

--- tests/history_versions_tests.py
+++ tests/history_versions_tests.py
@@ -7,10 +7,10 @@ from regparser.history.versions import Version
 
 class HistoryVersionsTest(TestCase):
     def test_order(self):
-        v1 = Version('1111', date(2001, 1, 1), date(2001, 1, 1))
-        v2 = Version('2222', date(2001, 1, 1), date(2002, 2, 2))
-        v3 = Version('3333', date(2003, 3, 3), date(2002, 2, 2))
-        v4 = Version('4444', date(2003, 3, 3), date(2002, 2, 2))
+        v1 = Version('first', date(2001, 1, 1), date(2001, 1, 1))
+        v2 = Version('eff', date(2001, 1, 1), date(2002, 2, 2))
+        v3 = Version('pub', date(2003, 3, 3), date(2002, 2, 2))
+        v4 = Version('pub >id', date(2003, 3, 3), date(2002, 2, 2))
 
         for permutation in permutations([v1, v2, v3, v4]):
             self.assertEqual(sorted(permutation), [v1, v2, v3, v4])
