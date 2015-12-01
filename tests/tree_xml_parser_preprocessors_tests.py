02770a264878c10538dede769692123d76b0cb09
Tadhg O'Higgins <tadhg.ohiggins@gsa.gov>
2015-11-30 18:29:21 -0800

    Make preprocessing of <SU> (footnote) elements consistent; separate the output with spaces. Fixes <https://github.com/18F/atf-eregs/issues>.

--- tests/tree_xml_parser_preprocessors_tests.py
+++ tests/tree_xml_parser_preprocessors_tests.py
@@ -249,17 +249,26 @@ class FootnotesTests(XMLBuilderMixin, TestCase):
     def test_split_comma_footnotes(self):
         """The XML will sometimes merge multiple references to footnotes into
         a single tag. Verify that they get split"""
-        with self.tree.builder("ROOT") as root:
-            root.P(_xml="Some content<SU>1</SU>")
-            root.P(_xml="More content<SU>2, 3, 4</SU>")
-            root.P(_xml="Last content<SU>5 6</SU>")
-
-        with self.assert_xml_transformed() as original_xml:
-            self.fn.split_comma_footnotes(original_xml)
+        def ftnt_compare(original, expected):
             with self.tree.builder("ROOT") as root:
-                root.P(_xml="Some content<SU>1</SU>")
-                root.P(_xml="More content<SU>2</SU>, <SU>3</SU>, <SU>4</SU>")
-                root.P(_xml="Last content<SU>5</SU> <SU>6</SU>")
+                root.P(_xml=original)
+
+            with self.assert_xml_transformed() as original_xml:
+                self.fn.split_comma_footnotes(original_xml)
+                with self.tree.builder("ROOT") as root:
+                    root.P(_xml=expected)
+
+        ftnt_compare("Some content<SU>1</SU>", "Some content<SU>1</SU>")
+        ftnt_compare("More content<SU>2, 3, 4</SU>",
+                     "More content<SU>2</SU> <SU>3</SU> <SU>4</SU>")
+        ftnt_compare("Even more content<SU>2,3,4</SU>",
+                     "Even more content<SU>2</SU> <SU>3</SU> <SU>4</SU>")
+        ftnt_compare("Yet more <SU>2</SU>, whatever<SU>3, 4</SU>",
+                     "Yet more <SU>2</SU>, whatever<SU>3</SU> <SU>4</SU>")
+        ftnt_compare("Penultimate content<SU>5 6</SU>",
+                     "Penultimate content<SU>5</SU> <SU>6</SU>")
+        ftnt_compare("Last content<SU>7</SU>, <SU>8</SU>",
+                     "Last content<SU>7</SU> <SU>8</SU>")
 
     def test_add_ref_attributes(self):
         """The XML elements which reference footnotes should be modified to
