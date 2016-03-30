e6df6fba416e6496a6f640c43921d344c18b51fc
CM Lubinski <christopher.lubinski@gsa.gov>
2016-03-30 15:14:37 +0000

    Allow null XML text
    We had been ensuring an empty string for all XML bodies built with the
    XMLBuilder. This isn't quite right, as
    <ATAG />
    is slightly different than
    <ATAG></ATAG>
    
    Allowing null XML texts broke a few lines of the parser, which have now been
    made more flexible.

--- regparser/tree/xml_parser/reg_text.py
+++ regparser/tree/xml_parser/reg_text.py
@@ -276,7 +276,7 @@ def build_from_section(reg_part, section_xml):
     section_nodes = []
     for section_number in section_nums:
         section_number = str(section_number)
-        section_text = section_xml.text.strip()
+        section_text = (section_xml.text or '').strip()
         tagged_section_text = section_xml.text
 
         section_title = u"§ " + reg_part + "." + section_number
