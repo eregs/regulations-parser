1fceb0a980f5c439822e32533a5bf8a84ddc2934
CM Lubinski <christopher.lubinski@gsa.gov>
2016-04-28 06:14:05 +0000

    Allow versions to be associated with proposals
    We had been assuming each Version structure would be tied to a _final_ rule,
    indicated by the presence of an effective date. This patch allows the
    effective date to be None, adds properties to indicate whether the version is
    associated with a final rule or proposal, and adds a consistent sorting
    mechanism for ordering both types of Versions.

--- regparser/history/versions.py
+++ regparser/history/versions.py
@@ -4,21 +4,53 @@ import json
 
 
 class Version(namedtuple('Version', ['identifier', 'published', 'effective'])):
+    @property
+    def is_final(self):
+        return bool(self.effective)
+
+    @property
+    def is_proposal(self):
+        return not self.is_final
+
     def json(self):
-        return json.dumps({'identifier': self.identifier,
-                           'published': self.published.isoformat(),
-                           'effective': self.effective.isoformat()})
+        result = {'identifier': self.identifier,
+                  'published': self.published.isoformat()}
+        if self.is_final:
+            result['effective'] = self.effective.isoformat()
+
+        return json.dumps(result)
 
     @staticmethod
     def from_json(json_str):
         json_dict = json.loads(json_str)
-        for key in ('published', 'effective'):
-            json_dict[key] = datetime.strptime(json_dict[key],
-                                               '%Y-%m-%d').date()
+        json_dict['published'] = datetime.strptime(json_dict['published'],
+                                                   '%Y-%m-%d').date()
+        effective = json_dict.get('effective')
+        if effective:
+            effective = datetime.strptime(effective, '%Y-%m-%d').date()
+        json_dict['effective'] = effective
         return Version(**json_dict)
 
     def __lt__(self, other):
-        """We'd like to sort in a slightly different field order"""
-        left = (self.effective, self.published, self.identifier)
-        right = (other.effective, other.published, other.identifier)
-        return left < right
+        """Linearizing versions requires knowing not only relevant dates and
+        identifiers, but also which versions are from final rules and which
+        are just proposals"""
+        if self.is_final and other.is_final:
+            left = (self.effective, self.published, self.identifier)
+            right = (other.effective, other.published, other.identifier)
+            return left < right
+        else:   # at least one of the two is a proposal
+            left = (self.published, self.identifier)
+            right = (other.published, other.identifier)
+            return left < right
+
+    @staticmethod
+    def parents_of(versions):
+        """A "parent" of a version is the version which it builds atop.
+        Versions can only build on final versions. Assume the versions are
+        already sorted"""
+        current_parent = None
+        for version in versions:
+            yield current_parent
+            if version.is_final:
+                current_parent = version
