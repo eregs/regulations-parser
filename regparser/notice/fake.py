c1e88d94addc33ae403f7b2757ac685fc7643768
CM Lubinski <christopher.lubinski@gsa.gov>
2016-05-11 18:28:30 +0000

    Merge conflict resolved

--- regparser/notice/fake.py
+++ regparser/notice/fake.py
@@ -5,7 +5,6 @@ from lxml import etree
 
 
 def build(doc_number, effective_on, cfr_title, cfr_part):
-<<<<<<< HEAD
     notice_xml = NoticeXML(etree.fromstring("""
         <ROOT>
             <PRTPAGE P="1" />
@@ -19,20 +18,3 @@ def build(doc_number, effective_on, cfr_title, cfr_part):
     notice_xml.published = effective_on
     notice_xml.cfr_refs = [TitlePartsRef(cfr_title, [cfr_part])]
     return notice_xml
-
-
-def effective_date_for(xml_tree):
-    """Return the date associated with an annual edition of regulation XML"""
-    nodes = xml_tree.xpath('//DATE') or xml_tree.xpath('//ORIGINALDATE')
-    return nodes[0].text
-=======
-    return {
-        "document_number": doc_number,
-        "effective_on": effective_on,
-        "initial_effective_on": effective_on,
-        "publication_date": effective_on,
-        "cfr_title": cfr_title,
-        "cfr_parts": [str(cfr_part)],   # for consistency w/ normal notices
-        "fr_url": None
-    }
->>>>>>> 258f6c3a336b74e74e0c8fc82c48e44a29d37d13
