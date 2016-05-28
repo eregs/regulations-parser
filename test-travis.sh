658add1f6aba2eb633454b39f8101e561459dfbb
CM Lubinski <cm.lubinski@gmail.com>
2016-05-28 12:54:06 -0400

    Run tests in all environments
    Also be sure to only submit to coveralls/integration results for one version
    of Python

--- test-travis.sh
+++ test-travis.sh
@@ -9,7 +9,7 @@ else
   ./integration_test.py uninstall
   ./integration_test.py install $INTEGRATION_TARGET
   ./integration_test.py build $INTEGRATION_TARGET
-  if [[ $TRAVIS_PULL_REQUEST = 'false' ]] && [[ $TRAVIS_BRANCH = 'master' ]]; then
+  if [[ $TRAVIS_PULL_REQUEST = 'false' ]] && [[ $TRAVIS_BRANCH = 'master' ]] && [[ $TRAVIS_PYTHON_VERSION = '2.7' ]]; then
     ./integration_test.py upload $INTEGRATION_TARGET
   else
     ./integration_test.py compare $INTEGRATION_TARGET
