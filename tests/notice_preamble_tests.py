77932c06b1310397d455abbc60053b5a2b960bd5
Tadhg O'Higgins <tadhg.ohiggins@gsa.gov>
2016-07-01 13:48:03 -0700

    Add tests for preamble sectionsa with both one and two paragaphs.

--- tests/notice_preamble_tests.py
+++ tests/notice_preamble_tests.py
@@ -13,10 +13,14 @@ class NoticePreambleTests(TestCase):
             ctx.P("ignored content")
             with ctx.SUM():
                 ctx.HD("SUMMARY:")
-                ctx.P("Out of order summary")
+                ctx.P("Out of order summary paragraph one")
+                ctx.P("Out of order summary paragraph two")
             with ctx.AGY():
                 ctx.HD("AGENCY:")
                 ctx.P("Agency name here.")
+            with ctx.SUM():
+                ctx.HD("SUMMARY 2:")
+                ctx.P("Out of order summary")
             with ctx.SUPLINF():
                 ctx.HED("Supp Inf")
                 ctx.P("P1")
@@ -48,10 +52,16 @@ class NoticePreambleTests(TestCase):
         self.assertEqual(root['intro']['p1'].title, 'SUMMARY:')
         self.assertEqual(root['intro']['p1'].text, '')
         self.assertEqual(root['intro']['p1']['p1'].text,
-                         'Out of order summary')
+                         'Out of order summary paragraph one')
+        self.assertEqual(root['intro']['p1']['p2'].text,
+                         'Out of order summary paragraph two')
         self.assertEqual(root['intro']['p2'].title, 'AGENCY:')
         self.assertEqual(root['intro']['p2'].text, '')
         self.assertEqual(root['intro']['p2']['p1'].text, 'Agency name here.')
+        self.assertEqual(root['intro']['p3'].title, 'SUMMARY 2:')
+        self.assertEqual(root['intro']['p3'].text, '')
+        self.assertEqual(root['intro']['p3']['p1'].text,
+                         'Out of order summary')
         self.assertEqual(root['p1'].text, 'P1')
         self.assertEqual(root['p2'].text, 'P2')
         self.assertEqual(root['I'].title, 'I. H1')
