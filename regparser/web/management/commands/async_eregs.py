ee7412c2c4a0fe14a259ee07f401a90fb296dc34
CM Lubinski <christopher.lubinski@gsa.gov>
2016-07-12 16:28:35 +0000

    Use a management command for async_eregs
    Migrates the previous file into a Django command

---
+++ regparser/web/management/commands/async_eregs.py
@@ -0,0 +1,89 @@
+from regparser.tasks import run_eregs_command
+
+from django.core.management.base import BaseCommand
+import django_rq
+from rq import registry
+from rq.queue import FailedQueue
+
+
+_OPTIONS = ('cfr_title', 'cfr_part', 'xml-ttl')
+_FLAGS = ('only-latest', 'unique', 'prompt')
+
+
+def argparse_to_click(eregs_args, **kwargs):
+    """Django uses argparse for parameters, but we use click. Hack a
+    conversion between the two"""
+    args = list(eregs_args)
+    for option in _OPTIONS:
+        underscored = option.replace('-', '_')
+        if kwargs.get(underscored):
+            args.append('--' + option)
+            args.append(kwargs[underscored])
+    for flag in _FLAGS:
+        underscored = flag.replace('-', '_')
+        if kwargs.get(underscored):
+            args.append('--' + flag)
+    return args
+
+
+def _print(write_fn, job):
+    logs = job.meta.get('logs', '')
+    log_length = len(logs.split('\n'))
+    write_fn("\t{}\n\t\tLogs:{}".format(job, log_length))
+
+
+def show_stats(write_fn):
+    """Print some metrics to stdout"""
+    queue = django_rq.get_queue()
+    conn = django_rq.get_connection()
+
+    write_fn("Queued:")
+    for job in queue.jobs:
+        _print(write_fn, job)
+
+    write_fn("Started:")
+    for job_id in registry.StartedJobRegistry(connection=conn).get_job_ids():
+        _print(write_fn, queue.fetch_job(job_id))
+
+    write_fn("Finished:")
+    for job_id in registry.FinishedJobRegistry(connection=conn).get_job_ids():
+        _print(write_fn, queue.fetch_job(job_id))
+
+    write_fn("Failed:")
+    for job in FailedQueue(connection=conn).jobs:
+        _print(write_fn, job)
+        for line in job.exc_info.split('\n'):
+            write_fn("\t\t" + line)
+
+
+class Command(BaseCommand):
+    help = """
+        Asynchronous `eregs` commands. To run,
+        1. pip install -r requirements_web.txt
+        2. start redis. For example, with docker:
+            docker run -p 6379:6379 -d redis
+        3. start a worker process
+            python manage.py rqworker
+        4. run an asynchronous command
+            python manage.py async_eregs pipeline 27 479 async_output_dir
+        5. check the status of your jobs:
+            python manage.py async_eregs    # no parameters
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument('eregs_args', nargs='*')
+        for option in _OPTIONS:
+            parser.add_argument('--' + option)
+        for flag in _FLAGS:
+            parser.add_argument('--' + flag, action='store_const', const=True)
+
+    def handle(self, *args, **kwargs):
+        eregs_args = argparse_to_click(**kwargs)
+        if not eregs_args:
+            show_stats(self.stdout.write)
+        else:
+            job = django_rq.enqueue(run_eregs_command, eregs_args,
+                                    # Run for at most half an hour
+                                    # Don't delete successes
+                                    timeout=60*30, result_ttl=-1)
+            self.stdout.write("OK: {}".format(job))
