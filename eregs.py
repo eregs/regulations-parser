925f537138a0743810be8d8d0a4ac412b6ebe477
CM Lubinski <christopher.lubinski@gsa.gov>
2016-08-01 19:26:08 +0000

    Set default Django settings when running eregs
    The execution path of calling `eregs` or `python eregs.py` wasn't setting a
    default settings module, which meant that accessing the settings would cause
    an explosion. Take a cue from manage.py, and set that value.

--- eregs.py
+++ eregs.py
@@ -1,5 +1,14 @@
-from regparser.web.management.commands.eregs import cli
+import os
+
+
+def main():
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "regparser.web.settings.dev")
+
+    from regparser.web.management.commands.eregs import cli
+
+    cli.main()
 
 
 if __name__ == '__main__':
-    cli.main()
+    main()
