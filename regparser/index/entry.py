fa26d94af93ce2c33d6ffa1fa3363f139f0df032
CM Lubinski <christopher.lubinski@gsa.gov>
2016-08-03 19:53:36 +0000

    Update dependency, entries to use database model
    Rather than storing file contents on disk, place them in the shared database

--- regparser/index/entry.py
+++ regparser/index/entry.py
@@ -11,6 +11,7 @@ from regparser.notice.xml import NoticeXML
 from regparser.tree.struct import (
     frozen_node_decode_hook, full_node_decode_hook, FullNodeEncoder)
 from regparser.tree.xml_parser.xml_wrapper import XMLWrapper
+from regparser.web.index.models import DependencyNode, Entry as DBEntry
 
 logger = logging.getLogger(__name__)
 
@@ -36,29 +37,20 @@ class Entry(object):
             prefix = os.path.join(prefix, self.PREFIX)
         return os.path.join(prefix, *self.path)
 
-    def _create_parent_dir(self):
-        """Create the requisite directories if needed"""
-        path = settings.EREGS_INDEX_ROOT
-        if self.PREFIX:
-            path = os.path.join(path, self.PREFIX)
-        path = os.path.join(path, *self.path[:-1])
-        if not os.path.exists(path):
-            os.makedirs(path)
-
     def write(self, content):
-        self._create_parent_dir()
-        with open(str(self), 'wb') as f:
-            f.write(self.serialize(content))
-            logger.info("Wrote {}".format(str(self)))
+        dep, _ = DependencyNode.objects.update_or_create(label=str(self))
+        DBEntry.objects.update_or_create(label=dep, defaults={
+            'contents': self.serialize(content)})
+        logger.info("Wrote {}".format(str(self)))
 
     def serialize(self, content):
         """Default implementation; treat content as bytes"""
         return content
 
     def read(self):
-        self._create_parent_dir()
-        with open(str(self), 'rb') as f:
-            return self.deserialize(f.read())
+        buffered_contents = DBEntry.objects.get(label=str(self)).contents
+        # load it all into memory -- @todo optimization point
+        return self.deserialize(bytes(buffered_contents))
 
     def deserialize(self, content):
         """Default implementation; treat the content as bytes"""
@@ -66,16 +58,20 @@ class Entry(object):
 
     def __iter__(self):
         """All sub-entries, i.e. the directory contents, as strings"""
-        if not os.path.exists(str(self)):
-            return iter([])
-        else:
-            return iter(sorted(os.listdir(str(self))))
+        # @todo optimization point: use db indexes/similar to speed up this
+        # query
+        self_path = str(self)
+        for entry in DBEntry.objects.filter(
+                label__label__startswith=self_path):
+            # Note: implicitly ordering by label in the DB model
+            if os.path.dirname(entry.label_id) == self_path:
+                yield os.path.basename(entry.label_id)
 
     def __len__(self):
         return len(list(self.__iter__()))
 
     def exists(self):
-        return os.path.exists(str(self))
+        return DBEntry.objects.filter(label=str(self)).exists()
 
 
 class Notice(Entry):
