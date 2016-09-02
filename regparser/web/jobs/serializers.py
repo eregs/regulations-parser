89500263d4e831db4e237aba391afd3429387a2f
Tadhg O'Higgins <tadhg.ohiggins@gsa.gov>
2016-09-01 17:05:15 -0700

    Clean up web API serializer code.

--- regparser/web/jobs/serializers.py
+++ regparser/web/jobs/serializers.py
@@ -8,6 +8,7 @@ from rest_framework import serializers
 
 
 class ParsingJobSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = ParsingJob
         fields = (
@@ -38,67 +39,30 @@ class ParsingJobSerializer(serializers.ModelSerializer):
         super(ParsingJobSerializer, self).save(**kwargs)
 
 
-class PipelineJobSerializer(serializers.ModelSerializer):
-    class Meta:
+class PipelineJobSerializer(ParsingJobSerializer):
+
+    class Meta(ParsingJobSerializer.Meta):
         model = PipelineJob
-        fields = (
-            "cfr_title",
-            "cfr_part",
-            "clear_cache",
-            "destination",  # Unsure about whether this should accept user
-                            # input or be set by the system.
-            "job_id",
-            "notification_email",
-            "only_latest",
-            "use_uploaded_metadata",
-            "use_uploaded_regulation",
-            "parser_errors",
-            "regulation_url",
-            "status",
-            "url"
-        )
-
-    # Fields we don't want user input for are listed below.
-    # For now, don't take user input for destination URL:
-    destination = serializers.URLField(read_only=True)
-    job_id = serializers.UUIDField(read_only=True)
-    parser_errors = serializers.CharField(read_only=True)
-    regulation_url = serializers.URLField(read_only=True)
-    status = serializers.CharField(read_only=True)
-    url = serializers.URLField(read_only=True)
+        fields = ParsingJobSerializer.Meta.fields + (
+                "cfr_title",
+                "cfr_part"
+            )
 
 
-class ProposalPipelineJobSerializer(serializers.ModelSerializer):
-    class Meta:
+class ProposalPipelineJobSerializer(ParsingJobSerializer):
+
+    class Meta(ParsingJobSerializer.Meta):
         model = ProposalPipelineJob
-        fields = (
-            "clear_cache",
-            "destination",  # Unsure about whether this should accept user
-                            # input or be set by the system.
+        fields = ParsingJobSerializer.Meta.fields + (
             "file_hexhash",
-            "job_id",
-            "notification_email",
-            "only_latest",
-            "use_uploaded_metadata",
-            "use_uploaded_regulation",
-            "parser_errors",
-            "regulation_url",
-            "status",
-            "url"
         )
 
     # Fields we don't want user input for are listed below.
-    # For now, don't take user input for destination URL:
-    destination = serializers.URLField(read_only=True)
     file_hexhash = serializers.CharField(max_length=32)
-    job_id = serializers.UUIDField(read_only=True)
-    parser_errors = serializers.CharField(read_only=True)
-    regulation_url = serializers.URLField(read_only=True)
-    status = serializers.CharField(read_only=True)
-    url = serializers.URLField(read_only=True)
 
 
 class FileUploadSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = RegulationFile
         fields = (
