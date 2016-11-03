13d6c8b629343149ed9300873aa297c27f634a10
CM Lubinski <cm.lubinski@gmail.com>
2016-11-03 10:34:50 -0400

    Move last function from "utils" module
    This function was only used in the `regparser.tree.interpretation` module, so
    move its logic there. Also simplifies the logic a bit and rewrites several
    related tests in pytest style.

--- regparser/utils.py
+++
@@ -1,6 +0,0 @@
-def title_body(text):
-    """Split text into its first line (the title) and the rest of the text."""
-    newline = text.find("\n")
-    if newline < 0:
-        return text, ""
-    return text[:newline], text[newline:]
