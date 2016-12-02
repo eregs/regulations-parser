8dd0a2ccc315b41dd71f31e901c2dd0492fd3a3a
CM Lubinski <cm.lubinski@gmail.com>
2016-12-02 04:46:23 +0000

    Convert interpretations tests to use py.test

--- tests/interpparser/layers_tests.py
+++ tests/interpparser/layers_tests.py
@@ -1,135 +1,128 @@
-from unittest import TestCase
-
 from regparser.tree.struct import Node
 from interpparser.layers import Interpretations
 
 
-class LayerInterpretationTest(TestCase):
-    def test_process(self):
-        root = Node(children=[
-            Node("Interp11a",
-                 [Node("child1"), Node("child2")],
-                 ['102', '11', 'a', Node.INTERP_MARK],
-                 node_type=Node.INTERP),
-            Node("Interp11c5v",
-                 label=['102', '11', 'c', '5', 'v', Node.INTERP_MARK],
-                 node_type=Node.INTERP),
-            Node("InterpB5ii",
-                 label=['102', 'B', '5', 'ii', Node.INTERP_MARK],
-                 node_type=Node.INTERP),
-            Node(children=[Node(children=[
+def test_process():
+    root = Node(children=[
+        Node("Interp11a",
+             [Node("child1"), Node("child2")],
+             ['102', '11', 'a', Node.INTERP_MARK],
+             node_type=Node.INTERP),
+        Node("Interp11c5v",
+             label=['102', '11', 'c', '5', 'v', Node.INTERP_MARK],
+             node_type=Node.INTERP),
+        Node("InterpB5ii",
+             label=['102', 'B', '5', 'ii', Node.INTERP_MARK],
+             node_type=Node.INTERP),
+        Node(children=[
+            Node(label=['102'], children=[
                 Node("Interp9c1",
                      label=['102', '9', 'c', '1', Node.INTERP_MARK],
                      node_type=Node.INTERP)
-                ], label=['102'])])
+            ])
         ])
+    ])
 
-        interp = Interpretations(root)
-        interp.pre_process()
-        interp11a = interp.process(Node(label=['102', '11', 'a']))
-        interp11c5v = interp.process(Node(
-            label=['102', '11', 'c', '5', 'v']
-        ))
-        interpB5ii = interp.process(Node(label=['102', 'B', '5', 'ii']))
-        interp9c1 = interp.process(Node(label=['102', '9', 'c', '1']))
+    interp = Interpretations(root)
+    interp.pre_process()
+    interp11a = interp.process(Node(label=['102', '11', 'a']))
+    interp11c5v = interp.process(Node(
+        label=['102', '11', 'c', '5', 'v']
+    ))
+    interpB5ii = interp.process(Node(label=['102', 'B', '5', 'ii']))
+    interp9c1 = interp.process(Node(label=['102', '9', 'c', '1']))
 
-        self.assertEqual(1, len(interp11a))
-        self.assertEqual(1, len(interp11c5v))
-        self.assertEqual(1, len(interpB5ii))
-        self.assertEqual(1, len(interp9c1))
-        self.assertEqual('102-11-a-Interp', interp11a[0]['reference'])
-        self.assertEqual('102-11-c-5-v-Interp', interp11c5v[0]['reference'])
-        self.assertEqual('102-B-5-ii-Interp', interpB5ii[0]['reference'])
-        self.assertEqual('102-9-c-1-Interp', interp9c1[0]['reference'])
-        self.assertEqual(None, interp.process(Node(
-            label=["102", "10", "a"])))
+    assert len(interp11a) == 1
+    assert len(interp11c5v) == 1
+    assert len(interpB5ii) == 1
+    assert len(interp9c1) == 1
+    assert interp11a[0]['reference'] == '102-11-a-Interp'
+    assert interp11c5v[0]['reference'] == '102-11-c-5-v-Interp'
+    assert interpB5ii[0]['reference'] == '102-B-5-ii-Interp'
+    assert interp9c1[0]['reference'] == '102-9-c-1-Interp'
+    assert interp.process(Node(label=['102', '10', 'a'])) is None
 
-    def test_process_subparagraph_of_referenced_text(self):
-        root = Node(children=[
-            Node("\n\n\n",
-                 [Node("Interp11a1",
-                       label=['100', '11', 'a', '1', Node.INTERP_MARK],
-                       node_type=Node.INTERP)],
-                 label=['100', '11', 'a', Node.INTERP_MARK],
-                 node_type=Node.INTERP)
-        ], label=['100'])
-        interp = Interpretations(root)
-        interp.pre_process()
-        self.assertEqual(None, interp.process(Node(
-            label=['100', '11', 'a']
-        )))
-        self.assertFalse(interp.process(Node(
-            label=['100', '11', 'a', '1'])) is None)
 
-    def test_process_has_multiple_paragraphs(self):
-        root = Node(children=[
-            Node("\n\n\n",
-                 [Node("Interp11a-1",
-                       label=['100', '11', 'a', Node.INTERP_MARK, '1'],
-                       node_type=Node.INTERP)],
-                 ['100', '11', 'a', Node.INTERP_MARK],
-                 node_type=Node.INTERP)
-            ], label=['100'])
-        interp = Interpretations(root)
-        interp.pre_process()
-        self.assertFalse(interp.process(Node(
-            label=["100", "11", "a"])) is None)
+def test_process_subparagraph_of_referenced_text():
+    root = Node(label=['100'], children=[
+        Node("\n\n\n",
+             node_type=Node.INTERP,
+             label=['100', '11', 'a', Node.INTERP_MARK],
+             children=[Node("Interp11a1", node_type=Node.INTERP,
+                            label=['100', '11', 'a', '1', Node.INTERP_MARK])])
+    ])
+    interp = Interpretations(root)
+    interp.pre_process()
+    assert interp.process(Node(label=['100', '11', 'a'])) is None
+    assert interp.process(Node(label=['100', '11', 'a', '1'])) is not None
 
-    def test_process_applies_to_multiple(self):
-        i1a = Node('Text', title='Paragraph 1(a) and 1(b)',
-                   label=['100', '1', 'a', Node.INTERP_MARK],
-                   node_type=Node.INTERP)
-        i1 = Node(label=['100', '1', Node.INTERP_MARK],
-                  node_type=Node.INTERP, children=[i1a])
-        root = Node(label=['100', Node.INTERP_MARK],
-                    node_type=Node.INTERP, children=[i1])
-        interp = Interpretations(root)
-        interp.pre_process()
-        self.assertFalse(interp.process(Node(label=['100', '1', 'a']))
-                         is None)
-        self.assertFalse(interp.process(Node(label=['100', '1', 'b']))
-                         is None)
 
-    def test_process_regressions(self):
-        i1a = Node('Text', title='Paragraph 1(a) and 1(b)',
-                   label=['100', '1', 'a', Node.INTERP_MARK])
-        interp = Interpretations(i1a)
-        interp.pre_process()
-        self.assertEqual(None, interp.process(Node(label=['100', '1', 'a'])))
+def test_process_has_multiple_paragraphs():
+    root = Node(label=['100'], children=[
+        Node("\n\n\n",
+             node_type=Node.INTERP,
+             label=['100', '11', 'a', Node.INTERP_MARK],
+             children=[Node("Interp11a-1", node_type=Node.INTERP,
+                            label=['100', '11', 'a', Node.INTERP_MARK, '1'])])
+    ])
+    interp = Interpretations(root)
+    interp.pre_process()
+    assert interp.process(Node(label=['100', '11', 'a'])) is not None
 
-        i1a1 = Node('Text', title='Paragraph 1(a) and 1(b)',
-                    label=['100', '1', 'a', Node.INTERP_MARK, '1'],
+
+def test_process_applies_to_multiple():
+    i1a = Node('Text', title='Paragraph 1(a) and 1(b)', node_type=Node.INTERP,
+               label=['100', '1', 'a', Node.INTERP_MARK])
+    i1 = Node(label=['100', '1', Node.INTERP_MARK], node_type=Node.INTERP,
+              children=[i1a])
+    root = Node(label=['100', Node.INTERP_MARK], node_type=Node.INTERP,
+                children=[i1])
+    interp = Interpretations(root)
+    interp.pre_process()
+    assert interp.process(Node(label=['100', '1', 'a'])) is not None
+    assert interp.process(Node(label=['100', '1', 'b'])) is not None
+
+
+def test_process_regressions():
+    i1a = Node('Text', title='Paragraph 1(a) and 1(b)',
+               label=['100', '1', 'a', Node.INTERP_MARK])
+    interp = Interpretations(i1a)
+    interp.pre_process()
+    assert interp.process(Node(label=['100', '1', 'a'])) is None
+
+    i1a1 = Node('Text', title='Paragraph 1(a) and 1(b)',
+                label=['100', '1', 'a', Node.INTERP_MARK, '1'],
+                node_type=Node.INTERP)
+    interp = Interpretations(i1a1)
+    interp.pre_process()
+    assert interp.process(Node(label=['100', '1', 'a'])) is None
+
+
+def test_empty_interpretations():
+    interp = Interpretations(None)
+    assert interp.empty_interpretation(Node('\n\n'))
+    assert interp.empty_interpretation(Node('', [Node('Subpar')]))
+    assert not interp.empty_interpretation(Node('Content'))
+    assert not interp.empty_interpretation(
+        Node('', [Node('Something', label=['1', Node.INTERP_MARK, '3'])]))
+
+
+def test_pre_process_multiple_interps():
+    interpG = Node('GGGG', title='Appendix G',
+                   label=['1111', 'G', 'Interp'], node_type=Node.INTERP)
+    interpH = Node('HHHH', title='Appendix H',
+                   label=['1111', 'H', 'Interp'], node_type=Node.INTERP)
+    interpGH = Node('GHGHGH', title='Appendices G and H',
+                    label=['1111', 'G_H', 'Interp'],
                     node_type=Node.INTERP)
-        interp = Interpretations(i1a1)
-        interp.pre_process()
-        self.assertEqual(None, interp.process(Node(label=['100', '1', 'a'])))
 
-    def test_empty_interpretations(self):
-        interp = Interpretations(None)
-        self.assertTrue(interp.empty_interpretation(Node('\n\n')))
-        self.assertTrue(interp.empty_interpretation(
-            Node('', [Node('Subpar')])))
-        self.assertFalse(interp.empty_interpretation(Node('Content')))
-        self.assertFalse(interp.empty_interpretation(
-            Node('', [Node('Something', label=['1', Node.INTERP_MARK, '3'])])))
+    tree = Node(label=['1111'], children=[
+        Node(label=['1111', 'Interp'], node_type=Node.INTERP, children=[
+            interpGH, interpG, interpH])])
 
-    def test_pre_process_multiple_interps(self):
-        interpG = Node('GGGG', title='Appendix G',
-                       label=['1111', 'G', 'Interp'], node_type=Node.INTERP)
-        interpH = Node('HHHH', title='Appendix H',
-                       label=['1111', 'H', 'Interp'], node_type=Node.INTERP)
-        interpGH = Node('GHGHGH', title='Appendices G and H',
-                        label=['1111', 'G_H', 'Interp'],
-                        node_type=Node.INTERP)
+    interp = Interpretations(tree)
+    interp.pre_process()
 
-        tree = Node(label=['1111'], children=[
-            Node(label=['1111', 'Interp'], node_type=Node.INTERP, children=[
-                interpGH, interpG, interpH])])
-
-        interp = Interpretations(tree)
-        interp.pre_process()
-
-        node = Node('App G', label=['1111', 'G'], node_type=Node.APPENDIX)
-        self.assertEqual(interp.process(node),
-                         [{'reference': '1111-G_H-Interp'},
-                          {'reference': '1111-G-Interp'}])
+    node = Node('App G', label=['1111', 'G'], node_type=Node.APPENDIX)
+    assert interp.process(node) == [{'reference': '1111-G_H-Interp'},
+                                    {'reference': '1111-G-Interp'}]
