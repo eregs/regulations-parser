5c1da67648b2c637f5c68dd1104a2a4f1678f00a
CM Lubinski <cm.lubinski@gmail.com>
2016-12-07 00:24:16 +0000

    Move existing preprocessors to entry_points
    This also allows us to remove some logic from regparser.plugins

--- setup.py
+++ setup.py
@@ -57,6 +57,30 @@ setup(
              "regparser.layer.external_citations:ExternalCitationParser"),
             "formatting = regparser.layer.formatting:Formatting",
             "graphics = regparser.layer.graphics:Graphics",
+        ],
+        "eregs_ns.parser.preprocessors": [
+            ("move-last-amdpar = "
+             "regparser.tree.xml_parser.preprocessors:MoveLastAMDPar"),
+            ("supplement-amd-par = "
+             "regparser.tree.xml_parser.preprocessors:SupplementAMDPar"),
+            ("parenthesis-cleanup = "
+             "regparser.tree.xml_parser.preprocessors:ParenthesesCleanup"),
+            ("move-adjoining-chars = "
+             "regparser.tree.xml_parser.preprocessors:MoveAdjoiningChars"),
+            ("approvals-fp = "
+             "regparser.tree.xml_parser.preprocessors:ApprovalsFP"),
+            ("extract-tags = "
+             "regparser.tree.xml_parser.preprocessors:ExtractTags"),
+            ("footnotes = "
+             "regparser.tree.xml_parser.preprocessors:Footnotes"),
+            ("parse-amdpars = "
+             "regparser.tree.xml_parser.preprocessors:ParseAMDPARs"),
+            ("atf-i-50032 = "
+             "regparser.tree.xml_parser.preprocessors:AtfI50032"),
+            ("atf-i-50031 = "
+             "regparser.tree.xml_parser.preprocessors:AtfI50031"),
+            ("atf-import-categories = "
+             "regparser.tree.xml_parser.preprocessors:ImportCategories"),
         ]
     }
 )
