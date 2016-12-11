e4cdfca38222fcf080d10493b532d494792ed76a
CM Lubinski <cm.lubinski@gmail.com>
2016-12-11 14:22:30 -0500

    Add build parameters flag to integration tests
    Also includes the attrs lib per @jacobian's suggestion

--- regparser/web/management/commands/integration_test.py
+++ regparser/web/management/commands/integration_test.py
@@ -6,9 +6,11 @@ fail the build. On passing builds against master, parse regulations and upload
 to cache as the current ground truth.
 """
 
+import functools
 import os.path
 import sys
 
+import attr
 import djclick as click
 import pip
 
@@ -16,38 +18,45 @@ from regparser.commands.compare_to import compare_to
 from regparser.web.management.commands.eregs import cli as eregs_cli
 
 
+@attr.attrs
+class Target(object):
+    title = attr.attrib()
+    parts = attr.attrib()
+    reqs = attr.attrib(default=attr.Factory(dict))
+    flags = attr.attrib(default=None)
+
+
 targets = {
-    'fec': {
-        'title': 11,
-        'parts': (
+    'fec': Target(
+        title=11,
+        parts=(
             [1, 2, 4, 5, 6, 7, 8] +
             list(range(100, 117)) + [200, 201, 300] +
             list(range(9001, 9009)) + [9012] + list(range(9031, 9040)) +
             [9405, 9407, 9409, 9410, 9411, 9420, 9428, 9430]
         ),
-    },
-    'atf': {
-        'title': 27,
-        'parts': [447, 478, 479, 555, 646],
-        'requirements': {
+        flags='--only-latest'
+    ),
+    'atf': Target(
+        title=27,
+        parts=[447, 478, 479, 555, 646],
+        reqs={
             'atf_regparser': '-e git+https://github.com/18F/atf-eregs.git#egg=atf_regparser&subdirectory=eregs_extensions',  # noqa
         },
-    },
-    'epa': {
-        'title': 40,
-        'parts': [262, 263, 264, 265, 271],
-        'requirements': {
+    ),
+    'epa': Target(
+        title=40,
+        parts=[262, 263, 264, 265, 271],
+        reqs={
             'epa_regparser': '-e git+https://github.com/18F/epa-notice.git#egg=epa_regparser&subdirectory=eregs_extensions',  # noqa
         },
-    },
+        flags='--only-latest'
+    ),
 }
 
 
-def get_paths(target):
-    return {
-        'cached_dir': os.path.join('tests', 'integration-data', target),
-        'output_dir': 'output-{}'.format(target),
-    }
+_ground_truth = functools.partial(os.path.join, 'tests', 'integration-data')
+_current_output = 'output-{}'.format
 
 
 @click.group()
@@ -59,14 +68,14 @@ def integration_test():
 @click.argument('target')
 def install(target):
     config = targets[target]
-    for requirement in config.get('requirements', {}).values():
+    for requirement in config.reqs.values():
         pip.main(['install', '--upgrade'] + requirement.split())
 
 
 @integration_test.command()
 def uninstall():
     for config in targets.values():
-        for package in config.get('requirements', {}).keys():
+        for package in config.reqs.keys():
             pip.main(['uninstall', '--yes', package])
 
 
@@ -74,22 +83,23 @@ def uninstall():
 @click.argument('target')
 def build(target):
     config = targets[target]
-    paths = get_paths(target)
 
-    for part in config['parts']:
-        eregs_cli('pipeline', str(config['title']), str(part),
-                  paths['output_dir'], '--only-latest')
+    for part in config.parts:
+        args = ['pipeline', str(config.title), str(part),
+                _current_output(target)]
+        if config.flags:
+            args.append(config.flags)
+        eregs_cli(*args)
 
 
 @integration_test.command()
 @click.argument('target')
 @click.pass_context
 def compare(ctx, target):
-    paths = get_paths(target)
     diffs = ctx.invoke(
         compare_to,
-        api_base=paths['cached_dir'],
-        paths=[paths['output_dir']],
+        api_base=_ground_truth(target),
+        paths=[_current_output(target)],
         prompt=False,
     )
     if diffs:
