876c37231f92c0a9420d469766d86fc9f7aca301
Robb Shecter <robb@weblaws.org>
2016-12-25 19:58:57 -0800

    Next failing test; CLI speedup

--- regparser/commands/derive_depths.py
+++ regparser/commands/derive_depths.py
@@ -7,10 +7,17 @@ logger = logging.getLogger(__name__)
 
 @click.command()
 @click.argument('markers', type=click.STRING, required=True)
-def derive_depths(marker_string):
+def derive_depths(markers) -> None:
     """
     Infer an outline's structure.
     Return a list of outline depths for a given list of comma-separated markers.
     """
-    return marker_string
+    print(markers)
 
+
+if __name__ == '__main__':
+    """Enable running this command directly. E.g.,
+    `$ python regparser/commands/derive_depths.py`. This can save 1.5 seconds or
+    more of startup time.
+    """
+    derive_depths()
--- tests/commands_derive_depths_tests.py
+++ tests/commands_derive_depths_tests.py
@@ -3,8 +3,13 @@ from click.testing import CliRunner
 from regparser.commands.derive_depths import derive_depths
 
 
-def test_produces_usage_message_with_no_args():
+def test_produces_usage_message_with_no_arg():
     result = CliRunner().invoke(derive_depths)
     assert result.exit_code == 2
     assert 'Usage' in result.output
 
+
+def test_returns_simple_result_for_a_simple_outline():
+    result = CliRunner().invoke(derive_depths, '(a),(b),(c)')
+    assert result.exit_code == 0
+    assert result.output == "[0, 0, 0]\n"
