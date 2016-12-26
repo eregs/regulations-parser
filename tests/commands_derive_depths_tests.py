97d3905f7384730b62f7e015092f44d6d28448fd
Robb Shecter <robb@weblaws.org>
2016-12-25 18:25:25 -0800

    Initial failing test

---
+++ tests/commands_derive_depths_tests.py
@@ -0,0 +1,9 @@
+from click.testing import CliRunner
+
+from regparser.commands.derive_depths import derive_depths
+
+
+def test_produces_usage_message_with_no_args():
+    result = CliRunner().invoke(derive_depths)
+    assert result.exit_code == 1
+    assert 'Usage' in result.output
