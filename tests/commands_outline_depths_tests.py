09eddbe2c62389e8b3763db7d993902b14445144
Robb Shecter <robb@weblaws.org>
2016-12-25 21:16:38 -0800

    Passing test for roman numerals

--- tests/commands_outline_depths_tests.py
+++ tests/commands_outline_depths_tests.py
@@ -13,3 +13,9 @@ def test_returns_simple_result_for_a_simple_outline():
     result = CliRunner().invoke(outline_depths, ['a b'])
     assert result.output == "0 0\n"
     assert result.exit_code == 0
+
+
+def test_handles_roman_numerals():
+    result = CliRunner().invoke(outline_depths, ['1 2 3 i ii iv 4 5 i A'])
+    assert result.output == "0 0 0 1 1 1 0 0 1 2\n"
+    assert result.exit_code == 0
