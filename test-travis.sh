1bfe49503be9f929cafa554489935e2a3de7f97d
CM Lubinski <cm.lubinski@gmail.com>
2017-03-12 13:10:39 -0400

    Add Docker build to the integration test matrix.
    We really only need to test building/executing the Docker image against one of
    the integration targets (as we're already testing Python 3.5), so this uses
    the fastest.

--- test-travis.sh
+++ test-travis.sh
@@ -1,15 +1,19 @@
 set -e
 set -x
 
-./manage.py migrate
-
 if [[ $INTEGRATION_TARGET = '' ]]; then
   py.test --cov-report term-missing --cov regparser
   flake8 .
 else
-  eregs clear
-  ./manage.py integration_test uninstall
-  ./manage.py integration_test install $INTEGRATION_TARGET
-  ./manage.py integration_test build $INTEGRATION_TARGET
-  ./manage.py integration_test compare $INTEGRATION_TARGET
+  if [[ $DOCKER_BUILD ]]; then
+    docker build . -t eregs-parser
+    MANAGE_CMD="docker run --rm -it -v eregs-cache:/app/cache -v output:/app/output --entrypoint ./manage.py eregs-parser"
+  else
+    MANAGE_CMD=./manage.py
+  fi
+  $MANAGE_CMD migrate
+  $MANAGE_CMD integration_test uninstall
+  $MANAGE_CMD integration_test install $INTEGRATION_TARGET
+  $MANAGE_CMD integration_test build $INTEGRATION_TARGET
+  $MANAGE_CMD integration_test compare $INTEGRATION_TARGET
 fi
