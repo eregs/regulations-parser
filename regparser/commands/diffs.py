0181ee526410aeb7e17cec85d284359c8ffbd063
CM Lubinski <cm.lubinski@gmail.com>
2017-04-16 23:18:41 -0400

    Refactor diffs command to use Django models.
    As a result, rewrites the tests.

--- regparser/commands/diffs.py
+++ regparser/commands/diffs.py
@@ -1,11 +1,24 @@
+import json
 import logging
 
 import click
 
 from regparser.diff.tree import changes_between
-from regparser.index import dependency, entry
+from regparser.tree.struct import frozen_node_decode_hook
+from regparser.web.index.models import Diff, Document
 
 logger = logging.getLogger(__name__)
+
+
+def decode(doc):
+    as_text = bytes(doc.contents).decode('utf-8')
+    return json.loads(as_text, object_hook=frozen_node_decode_hook)
+
+
+def encode(diff):
+    encoder = json.JSONEncoder(sort_keys=True, indent=4)
+    as_text = encoder.encode(diff)
+    return as_text.encode('utf-8')     # as bytes
 
 
 @click.command()
@@ -14,24 +27,21 @@ logger = logging.getLogger(__name__)
 def diffs(cfr_title, cfr_part):
     """Construct diffs between known trees."""
     logger.info("Build diffs - %s Part %s", cfr_title, cfr_part)
-    tree_dir = entry.FrozenTree(cfr_title, cfr_part)
-    diff_dir = entry.Diff(cfr_title, cfr_part)
-    pairs = [(l.path[-1], r.path[-1])
-             for l in tree_dir.sub_entries()
-             for r in tree_dir.sub_entries()]
-    deps = dependency.Graph()
-    for lhs_id, rhs_id in pairs:
-        deps.add(diff_dir / lhs_id / rhs_id, tree_dir / lhs_id)
-        deps.add(diff_dir / lhs_id / rhs_id, tree_dir / rhs_id)
 
+    docs = Document.objects.select_related('version').filter(
+        version__cfr_title=cfr_title, version__cfr_part=cfr_part)
+    pairs = [(lhs, rhs) for lhs in docs for rhs in docs]
     trees = {}
-    for lhs_id, rhs_id in pairs:
-        path = diff_dir / lhs_id / rhs_id
-        deps.validate_for(path)
-        if deps.is_stale(path):
-            if lhs_id not in trees:
-                trees[lhs_id] = (tree_dir / lhs_id).read()
-            if rhs_id not in trees:
-                trees[rhs_id] = (tree_dir / rhs_id).read()
 
-            path.write(dict(changes_between(trees[lhs_id], trees[rhs_id])))
+    for lhs, rhs in pairs:
+        lhs_id = lhs.version.identifier
+        rhs_id = rhs.version.identifier
+        if not Diff.objects.filter(left_document=lhs, right_document=rhs):
+            if lhs_id not in trees:
+                trees[lhs_id] = decode(lhs)
+            if rhs_id not in trees:
+                trees[rhs_id] = decode(rhs)
+
+            diff = dict(changes_between(trees[lhs_id], trees[rhs_id]))
+            Diff.objects.create(left_document=lhs, right_document=rhs,
+                                contents=encode(diff))
