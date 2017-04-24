8a6754decdc695af282aacdea447705a74bceb26
CM Lubinski <cm.lubinski@gmail.com>
2017-04-24 19:55:02 -0400

    Refactor fill_with_rules command to Django models

--- regparser/commands/fill_with_rules.py
+++ regparser/commands/fill_with_rules.py
@@ -1,69 +1,55 @@
+import json
 import logging
 from collections import defaultdict
+from itertools import dropwhile
 
 import click
 
-from regparser.index import dependency, entry
 from regparser.notice.compiler import compile_regulation
 from regparser.notice.xml import NoticeXML
-from regparser.web.index.models import CFRVersion
+from regparser.tree.struct import FullNodeEncoder, frozen_node_decode_hook
+from regparser.web.index.models import CFRVersion, Document
 
 logger = logging.getLogger(__name__)
 
 
-def drop_initial_orphans(versions_with_parents, existing):
-    """We can only build a version if there's a complete tree before it to
-    build from. As such, we need to drop any orphaned versions from the
-    beginning of our list"""
-    for idx, (version, _) in enumerate(versions_with_parents):
-        if version.identifier in existing:
-            return versions_with_parents[idx:]
-        logger.warning("No previous annual edition to version %s; ignoring",
-                       version.identifier)
-    return []
+def build_pairs(cfr_title, cfr_part):
+    """Find all pairs of versions such that the first of each pair needs to
+    (and can) be built and the second is the version that it builds on."""
+    query = CFRVersion.objects.select_related('source').filter(
+        cfr_title=cfr_title, cfr_part=cfr_part)
+
+    versions = [v for v in sorted(query)]
+    version_pairs = zip(versions, CFRVersion.parents_of(versions))
+    version_pairs = dropwhile(lambda (v, p): p is None, version_pairs)
+    version_pairs = dropwhile(lambda (v, p): not p.has_doc(), version_pairs)
+    return [(v, p) for v, p in version_pairs if not v.has_doc()]
 
 
-def dependencies(tree_dir, version_dir, versions_with_parents):
-    """Set up the dependency graph for this regulation. First calculates
-    "gaps" -- versions for which there is no existing tree. In this
-    calculation, we ignore the first version, as we won't be able to build
-    anything for it. Add dependencies for any gaps, tying the output tree to
-    the preceding tree, the version info and the parsed rule"""
-    existing_tree_ids = {tree.path[-1] for tree in tree_dir.sub_entries()}
-    version_pairs = drop_initial_orphans(
-        versions_with_parents, existing_tree_ids)
-    gaps = [(version, parent) for (version, parent) in version_pairs
-            if version.identifier not in existing_tree_ids]
-
-    deps = dependency.Graph()
-    for version, parent in gaps:
-        doc_number = version.identifier
-        deps.add(tree_dir / doc_number, tree_dir / parent.identifier)
-        deps.add(tree_dir / doc_number, entry.Notice(doc_number))
-        deps.add(tree_dir / doc_number, version_dir / doc_number)
-    return deps
+def doc_to_tree(doc):
+    as_text = bytes(doc.contents).decode('utf-8')
+    return json.loads(as_text, object_hook=frozen_node_decode_hook)
 
 
-def is_derived(version_id, deps, tree_dir):
-    """We only want to process trees which are created by parsing rules. To do
-    that, we'll filter by those trees which have a dependency on a parsed
-    rule"""
-    tree = str(tree_dir / version_id)
-    notice = str(entry.Notice(version_id))
-    return notice in deps.dependencies(tree)
+def tree_to_bytes(tree):
+    encoder = FullNodeEncoder(sort_keys=True, indent=4)
+    return encoder.encode(tree).encode('utf-8')     # as bytes
 
 
-def process(tree_path, previous, version_id):
-    """Build and write a tree by combining the preceding tree with changes
-    present in the associated rule"""
-    prev_tree = (tree_path / previous).read()
-    notice = NoticeXML.from_db(version_id)
+def save_tree(version, parent_doc):
+    """Given a CFRVersion and the document of the version it builds off,
+    compile a new version of the tree and save it"""
+    notice = NoticeXML(version.source.xml())
     notice_changes = defaultdict(list)
     for amendment in notice.amendments:
         for label, change_list in amendment.get('changes', []):
             notice_changes[label].extend(change_list)
+    prev_tree = doc_to_tree(parent_doc)
     new_tree = compile_regulation(prev_tree, notice_changes)
-    (tree_path / version_id).write(new_tree)
+    version.doc = Document.objects.create(
+        collection='gpo_cfr', label=new_tree.label[0],
+        source=version.source, version=version, previous_document=parent_doc,
+        contents=tree_to_bytes(new_tree))
 
 
 @click.command()
@@ -74,20 +60,6 @@ def fill_with_rules(cfr_title, cfr_part):
     cannot be derived through annual editions, it must be built by parsing the
     changes in final rules. This command builds those missing trees"""
     logger.info("Fill with rules - %s CFR %s", cfr_title, cfr_part)
-    tree_dir = entry.Tree(cfr_title, cfr_part)
-    version_dir = entry.Version(cfr_title, cfr_part)
-    query = CFRVersion.objects.filter(cfr_title=cfr_title, cfr_part=cfr_part)
 
-    versions = [v for v in sorted(query)]
-    versions_with_parents = list(zip(versions,
-                                     CFRVersion.parents_of(versions)))
-    deps = dependencies(tree_dir, version_dir, versions_with_parents)
-
-    derived = [(version.identifier, parent.identifier)
-               for version, parent in versions_with_parents
-               if is_derived(version.identifier, deps, tree_dir) and parent]
-    for version_id, parent_id in derived:
-        deps.validate_for(tree_dir / version_id)
-        if deps.is_stale(tree_dir / version_id):
-            process(tree_dir, parent_id, version_id)
-            deps.rebuild()
+    for version, parent_version in build_pairs(cfr_title, cfr_part):
+        save_tree(version, parent_version.doc)
